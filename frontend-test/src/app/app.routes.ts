2bb76bbd70768fed443bdfc39ca780fabc4f8a4d
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2026-01-29 18:53:37 +0700

    fix: removed .git from subfolders

---
+++ frontend-test/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MyComponent } from './features/my-component/my-component';
+
+export const routes: Routes = [
+    {
+        path: "my-component",
+        component: MyComponent
+    }
+];
